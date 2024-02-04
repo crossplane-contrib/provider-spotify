ce8ba716ffeaf016a6183b0e6ec519c21480d575
Theo Chatzimichos <tampakrap@gmail.com>
2024-02-04 16:35:43 +0100

    run make generate

--- apis/zz_register.go
+++ apis/zz_register.go
@@ -14,7 +14,7 @@ package apis
 import (
 	"k8s.io/apimachinery/pkg/runtime"
 
-	v1alpha1 "github.com/tampakrap/provider-spotify/apis/null/v1alpha1"
+	v1alpha1 "github.com/tampakrap/provider-spotify/apis/playlist/v1alpha1"
 	v1alpha1apis "github.com/tampakrap/provider-spotify/apis/v1alpha1"
 	v1beta1 "github.com/tampakrap/provider-spotify/apis/v1beta1"
 )
--- internal/controller/zz_setup.go
+++ internal/controller/zz_setup.go
@@ -9,7 +9,7 @@ import (
 
 	"github.com/crossplane/upjet/pkg/controller"
 
-	resource "github.com/tampakrap/provider-spotify/internal/controller/null/resource"
+	playlist "github.com/tampakrap/provider-spotify/internal/controller/playlist/playlist"
 	providerconfig "github.com/tampakrap/provider-spotify/internal/controller/providerconfig"
 )
 
@@ -17,7 +17,7 @@ import (
 // the supplied manager.
 func Setup(mgr ctrl.Manager, o controller.Options) error {
 	for _, setup := range []func(ctrl.Manager, controller.Options) error{
-		resource.Setup,
+		playlist.Setup,
 		providerconfig.Setup,
 	} {
 		if err := setup(mgr, o); err != nil {
