3df3a4db9eb738b22bd8f4489913483d63a5eb0c
Theo Chatzimichos <tampakrap@gmail.com>
2024-03-30 19:04:39 +0100

    fix: hopefully last CRD_ROOT_GROUP fixes

--- cluster/test/setup.sh
+++ cluster/test/setup.sh
@@ -3,17 +3,17 @@ set -aeuo pipefail
 
 echo "Running setup.sh"
 echo "Creating cloud credential secret..."
-${KUBECTL} -n upbound-system create secret generic provider-secret --from-literal=credentials="${UPTEST_CLOUD_CREDENTIALS}" --dry-run=client -o yaml | ${KUBECTL} apply -f -
+${KUBECTL} -n crossplane-system create secret generic provider-secret --from-literal=credentials="${UPTEST_CLOUD_CREDENTIALS}" --dry-run=client -o yaml | ${KUBECTL} apply -f -
 
 echo "Waiting until provider is healthy..."
 ${KUBECTL} wait provider.pkg --all --for condition=Healthy --timeout 5m
 
 echo "Waiting for all pods to come online..."
-${KUBECTL} -n upbound-system wait --for=condition=Available deployment --all --timeout=5m
+${KUBECTL} -n crossplane-system wait --for=condition=Available deployment --all --timeout=5m
 
 echo "Creating a default provider config..."
 cat <<EOF | ${KUBECTL} apply -f -
-apiVersion: spotify.upbound.io/v1beta1
+apiVersion: spotify.crossplane.io/v1beta1
 kind: ProviderConfig
 metadata:
   name: default
@@ -22,5 +22,5 @@ spec:
     source: Secret
     secretRef:
       name: provider-secret
-      namespace: upbound-system
+      namespace: crossplane-system
       key: credentials
